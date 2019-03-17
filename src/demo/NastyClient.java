1d4002ef80b2ae921ffd65c2e24016b72533a71b
authentical <cvspencer@gmail.com>
2019-03-17 20:49:01 +0900

    Added NastyClient that DOS's the MultiThreadedBlockingServer
    Added ThreadMaker that is supposed to help determine how many threads
    a system can actually make before blowing up.

---
+++ src/demo/NastyClient.java
@@ -0,0 +1,16 @@
+package demo;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class NastyClient {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Socket[] sockets = new Socket[3000];
+        for(int i=0; i< sockets.length; i++){
+            sockets[i] = new Socket("localhost", 8010);
+        }
+
+        Thread.sleep(100_000);
+    }
+}
---
+++ src/demo/ThreadMaker.java
@@ -0,0 +1,26 @@
+package demo;
+
+
+// Instructor suggested using this code to help find out what the max threads
+// that could be created on a system... but I let it run for 5 minutes, got 165_000 threads
+// and JVM memory backed off. No conclusion yet.
+
+
+public class ThreadMaker {
+
+    public static void main(String[] args) {
+
+
+        var num = new java.util.concurrent.atomic.AtomicInteger();
+        final long startTime = System.currentTimeMillis();
+
+        var pool = java.util.concurrent.Executors.newCachedThreadPool();
+        while(true) {
+            pool.submit(() -> {
+                System.out.println(num.incrementAndGet() + " threads submitted. " + (System.currentTimeMillis() - startTime) + "ms");
+                Thread.sleep(10_000_000);
+                return null;
+            });
+        }
+    }
+}
