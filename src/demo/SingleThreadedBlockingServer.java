d2b1ce3368be45a08795901ea69dd324b578cbed
authentical <cvspencer@gmail.com>
2019-03-17 19:12:50 +0900

    Finished SingleThreadedBlockingServer

--- src/demo/SingleThreadedBlockingServer.java
+++ src/demo/SingleThreadedBlockingServer.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class SingleThreadedBlockingServer {
 
@@ -14,11 +15,39 @@ public class SingleThreadedBlockingServer {
 
         while(true){
 
-            Socket clientSock = serverSock.accept();
-            InputStream in = clientSock.getInputStream();
-            OutputStream out = clientSock.getOutputStream();
+            Socket client = serverSock.accept();
 
-
+            handle(client);
         }
+    }
+
+    private static void handle(Socket client) throws IOException{
+
+
+        System.out.println("Connected to " + client);
+
+        try(client;
+            InputStream in = client.getInputStream();
+            OutputStream out = client.getOutputStream();
+            )
+        {
+
+            int data;
+
+            while ((data = in.read()) != -1) {
+
+                out.write(xorAlphas(data));  // Write transmogrified data
+            }
+
+        } finally {
+            System.out.println(client + " disconnected.");
+        }
+    }
+
+    private static int xorAlphas(int data){
+
+        return Character.isLetter(data) ? data ^ ' ' : data;
+        // If its a letter, return data XOR ' ', if not letter, return data
+
     }
 }
